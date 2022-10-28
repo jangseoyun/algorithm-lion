1c6c4849f77974be135f4cb37cf60dea6eaec9c3
seoyun <may-primavera@naver.com>
2022-10-28 10:47:30 +0900

    feat: 전화번호 목록
    - 최악의 상황을 먼저 생각하기

---
+++ src/main/java/com/algorithm/day22/Pokemon02.java
@@ -0,0 +1,13 @@
+package com.algorithm.day22;
+
+public class Pokemon02 {
+    public int solution(int[] arr) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Pokemon02 T = new Pokemon02();
+        int[] arr = {3,3,3,2,2,2};
+        System.out.println(T.solution(arr));
+    }
+}
