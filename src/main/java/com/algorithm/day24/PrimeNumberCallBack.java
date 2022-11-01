a503542936be56856c6c4f86674b8fa49674afc0
seoyun <may-primavera@naver.com>
2022-11-01 10:47:07 +0900

    refact: template callback 람다 적용

--- src/main/java/com/algorithm/day24/PrimeNumberCallBack.java
+++ src/main/java/com/algorithm/day24/PrimeNumberCallBack.java
@@ -11,6 +11,7 @@ public class PrimeNumberCallBack {
         // i * i < n
         // Math.sqrt(n) 루트 구하기 (연산이 많다)
         for (int i = 2; stmt.compare(i, n); i++) {
+            System.out.println(i);
             if (n % i == 0) {
                 return false;
             }
@@ -20,8 +21,10 @@ public class PrimeNumberCallBack {
 
     public static void main(String[] args) {
         PrimeNumberCallBack T = new PrimeNumberCallBack();
-        boolean r = T.isPrime(13);
-        System.out.println(r);
+        boolean r1 = T.isPrime(13, (a, b) -> a < b);
+        boolean r2 = T.isPrime(17, (a, b) -> a < b / 2);
+        boolean r3 = T.isPrime(19, (a, b) -> a * a < b);
+        System.out.printf("%s %s %s", r1, r2, r3);
     }
 
 }
