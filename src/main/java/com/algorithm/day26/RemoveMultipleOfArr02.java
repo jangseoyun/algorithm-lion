79e136b2f50257aa2110e6125aed6bb45de1e613
seoyun <may-primavera@naver.com>
2022-11-03 10:13:52 +0900

    fact: (에라토스테네스 체) 소수만 출력

--- src/main/java/com/algorithm/day26/RemoveMultipleOfArr02.java
+++ src/main/java/com/algorithm/day26/RemoveMultipleOfArr02.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 //4, 6
 //5, 8
 public class RemoveMultipleOfArr02 {
-    public int solution(int n) {
+    public void solution(int n) {
         n = 50; // n은 50개
         int[] nums = new int[n - 1]; // n-1개 만큼 2~50개까지 총 49개
         boolean[] checks = new boolean[nums.length]; // nums만큼 checks 생성
@@ -17,12 +17,25 @@ public class RemoveMultipleOfArr02 {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = i + 2;//2~50까지
         }
+
+        //2의 배수 지우기
+        int multipleOf = 2;
+        for (int i = 2; i < nums.length; i += multipleOf) {
+            checks[i] = false;
+        }
+
+        //check를 참고해서 true인 nums를 출력
+        for (int i = 0; i < checks.length; i++) {
+            if (checks[i] == true) {
+                System.out.print(nums[i] + " ");
+            }
+        }
     }
 
     public static void main(String[] args) {
         RemoveMultipleOfArr02 T = new RemoveMultipleOfArr02();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(T.solution(n));
+        T.solution(n);
     }
 }
