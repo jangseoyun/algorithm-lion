a4e45b8446ea6494828cba3b732114a133ef4235
seoyun <may-primavera@naver.com>
2022-11-03 10:15:03 +0900

    feat: (에라토스테네스 체) 소수만 출력

--- src/main/java/com/algorithm/day26/RemoveMultipleOfArr02.java
+++ src/main/java/com/algorithm/day26/RemoveMultipleOfArr02.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 //5, 8
 public class RemoveMultipleOfArr02 {
     public void solution(int n) {
-        n = 50; // n은 50개
-        int[] nums = new int[n - 1]; // n-1개 만큼 2~50개까지 총 49개
-        boolean[] checks = new boolean[nums.length]; // nums만큼 checks 생성
+        n = 50; //n은 50개
+        int[] nums = new int[n - 1]; //n-1개 만큼 2~50개까지 총 49개
+        boolean[] checks = new boolean[nums.length]; //nums만큼 checks 생성
         Arrays.fill(checks, true); //checks를 true로 초기와 : 기본값 true
 
         for (int i = 0; i < nums.length; i++) {
