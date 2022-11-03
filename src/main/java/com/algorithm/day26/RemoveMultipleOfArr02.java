bdb38b58e4dfb6cfb58867203997e638ae3ae21d
seoyun <may-primavera@naver.com>
2022-11-03 09:33:26 +0900

    faat: (에라토스테네스 체) 실습, 배수찾기

--- src/main/java/com/algorithm/day26/RemoveMultipleOfArr02.java
+++ src/main/java/com/algorithm/day26/RemoveMultipleOfArr02.java
@@ -8,17 +8,19 @@ import java.util.Scanner;
 //4, 6
 //5, 8
 public class RemoveMultipleOfArr02 {
-    public int solution(int n){
+    public int solution(int n) {
         int cnt = 0;
 
         for (int i = 2; i < n; i++) {
-            for (int j = 2; j < n; j++) {
-
+            System.out.print("i: " + i);
+            for (int j = i; j < n; j = j + i) {//베수 출력
+                System.out.println(" j: " + j);
             }
         }
 
         return cnt;
     }
+
     public static void main(String[] args) {
         RemoveMultipleOfArr02 T = new RemoveMultipleOfArr02();
         Scanner sc = new Scanner(System.in);
