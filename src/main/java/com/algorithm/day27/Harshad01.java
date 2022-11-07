b975c70577e0201568090a4981fbc8de874fb28f
seoyun <may-primavera@naver.com>
2022-11-07 09:10:09 +0900

    feat: (하샤드의 수) 자릿수의 합 구하기

---
+++ src/main/java/com/algorithm/day27/Harshad01.java
@@ -0,0 +1,13 @@
+package com.algorithm.day27;
+
+public class Harshad01 {
+    public boolean solution(int x) {
+        boolean answer = true;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Harshad01 T = new Harshad01();
+        int input = 11;
+        System.out.println(T.solution(input));
+    }
+}
