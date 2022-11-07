76503b6378169d8b2942f6a7d74ef33bfa813ed8
seoyun <may-primavera@naver.com>
2022-11-07 09:31:12 +0900

    feat: (하샤드의 수) 완료

--- src/main/java/com/algorithm/day27/Harshad01.java
+++ src/main/java/com/algorithm/day27/Harshad01.java
@@ -2,12 +2,26 @@ package com.algorithm.day27;
 
 public class Harshad01 {
     public boolean solution(int x) {
-        boolean answer = true;
+        boolean answer = false;
+        // 나머지가 마지막 수
+        // 10으로 나누기
+        int sum = 0;
+        int tmp = x;
+        while (tmp > 0) {
+            sum += tmp % 10;
+            tmp = tmp / 10;
+        }
+        System.out.println(sum);
+
+        if (x % sum == 0) {
+            answer = true;
+        }
+
         return answer;
     }
     public static void main(String[] args) {
         Harshad01 T = new Harshad01();
-        int input = 11;
+        int input = 13;
         System.out.println(T.solution(input));
     }
 }
