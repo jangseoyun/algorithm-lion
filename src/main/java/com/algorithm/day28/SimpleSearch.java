c2206f506b74d1a422b33d3c70795c9210793331
seoyun <may-primavera@naver.com>
2022-11-08 09:06:36 +0900

    feat: (데이터 탐색) 단순탐색 초기 세팅

---
+++ src/main/java/com/algorithm/day28/SimpleSearch.java
@@ -0,0 +1,22 @@
+package com.algorithm.day28;
+
+import java.util.Scanner;
+
+public class SimpleSearch {
+    public int solution() {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        SimpleSearch T = new SimpleSearch();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(T.solution());
+    }
+}
