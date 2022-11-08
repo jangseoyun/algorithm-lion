1433ce37e734a9ceffb054f696f1974ba8e90369
seoyun <may-primavera@naver.com>
2022-11-08 09:18:29 +0900

    refact: (데이터 탐색) for문 break 추가

--- src/main/java/com/algorithm/day28/SimpleSearch.java
+++ src/main/java/com/algorithm/day28/SimpleSearch.java
@@ -8,6 +8,7 @@ public class SimpleSearch {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == k) {
                 answer = i + 1;
+                break;
             }
         }
 
