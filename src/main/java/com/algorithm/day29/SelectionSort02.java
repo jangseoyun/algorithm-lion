e98f97c1c291737c7308bd49ea2ffd0b85254b7b
seoyun <may-primavera@naver.com>
2022-11-09 10:54:37 +0900

    commit: 함수형 인터페이스 사용

--- src/main/java/com/algorithm/day29/SelectionSort02.java
+++ src/main/java/com/algorithm/day29/SelectionSort02.java
@@ -1,18 +1,23 @@
 package com.algorithm.day29;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 
-interface SortStrategy {
+/*interface SortStrategy {
     boolean apply(int a, int b);
-}
+}*/
+
+
 
 public class SelectionSort02 {
 
-    public int[] selectionSort(int[] arr, SortStrategy stmt) {
+    public int[] selectionSort(int[] arr, BiFunction<Integer, Integer, Boolean> biFunction ) {
         for (int i = 0; i < arr.length - 1; i++) {
             int swapIdx = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (stmt.apply(arr[swapIdx], arr[j])) {
+                if (biFunction.apply(arr[swapIdx], arr[j])) {
                     swapIdx = j;
                 }
             }
@@ -31,5 +36,15 @@ public class SelectionSort02 {
 
         T.selectionSort(arr, (a, b) -> a < b);//내림차순
         T.selectionSort(arr, (a, b) -> a > b);//오름차순
+
+        //함수형 인터페이스를 통해서 input과 retrun 타입을 지정하여 연산 가능
+        Function<Integer[], Boolean> sortInterface = (arr1) -> arr1[0] > arr1[1];
+        System.out.println(sortInterface.apply(new Integer[]{10, 20}));
+
+        //앞의 integer a,b 를 받아서 연산을 한 결과를 boolean형으로 리턴한다
+        BiFunction<Integer, Integer, Boolean> biFunction =  (a, b) -> a > b;
+
+        //True or False(Boolean)를 리턴하는 Interface
+        BiPredicate<Integer, Integer> biPredicate = (a, b) -> a > b;
     }
 }
