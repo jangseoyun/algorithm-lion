edef86f105ea344c1e4b5bc98e4ffef5e6c64b54
seoyun <may-primavera@naver.com>
2022-11-09 09:45:09 +0900

    feat: (선택 정렬) 오름차순 정렬 완료

--- src/main/java/com/algorithm/day29/SelectionSort.java
+++ src/main/java/com/algorithm/day29/SelectionSort.java
@@ -4,18 +4,23 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public int[] solution(int[] arr) {
-        int minValue = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 1; j < arr.length; j++) {
-                if (minValue > arr[i]) {
-                    minValue = arr[i];
-                    arr[i] = minValue;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIdx] > arr[j]) {
+                    minIdx = j;
                 }
             }
+            int tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
+            System.out.println(Arrays.toString(arr));
         }
-        System.out.println(Arrays.toString(arr));
+
         return arr;
     }
+
     public static void main(String[] args) {
         SelectionSort T = new SelectionSort();
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
