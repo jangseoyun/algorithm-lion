17faa95772c2d7cf5208d7331264d3bcfeb3c11b
seoyun <may-primavera@naver.com>
2022-11-10 09:06:14 +0900

    feat: [알고리즘] 같은 숫자는 싫어 초기 세팅

---
+++ src/main/java/com/algorithm/day30/SameNumber.java
@@ -0,0 +1,14 @@
+package com.algorithm.day30;
+
+public class SameNumber {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        SameNumber T = new SameNumber();
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+        System.out.println(T.solution(arr));
+    }
+}
