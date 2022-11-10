fdc456bafd7cef747168b0f6c34ca19b74a60478
seoyun <may-primavera@naver.com>
2022-11-10 09:23:54 +0900

    feat: [알고리즘] 같은 숫자는 싫어 list 사용

--- src/main/java/com/algorithm/day30/SameNumber.java
+++ src/main/java/com/algorithm/day30/SameNumber.java
@@ -1,14 +1,33 @@
 package com.algorithm.day30;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SameNumber {
-    public int[] solution(int []arr) {
-        int[] answer = {};
+    public List<Integer> solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                answer.add(arr[i]);
+                i++;
+            } else {
+                answer.add(arr[i]);
+            }
+        }
+
+        for (Integer integer : answer) {
+            System.out.println(integer);
+        }
 
         return answer;
     }
+
     public static void main(String[] args) {
         SameNumber T = new SameNumber();
         int[] arr = {1, 1, 3, 3, 0, 1, 1};
-        System.out.println(T.solution(arr));
+        for (int num : T.solution(arr)) {
+            System.out.println(num);
+        }
     }
 }
