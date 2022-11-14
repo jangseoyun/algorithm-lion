6fae7199fd0190be9023c06a8fbcb3f8726a2a99
seoyun <may-primavera@naver.com>
2022-11-14 11:44:59 +0900

    feat: [알고리즘] 에라토스테네스의 체

--- src/main/java/com/algorithm/day31/PrimeNumber.java
+++ src/main/java/com/algorithm/day31/PrimeNumber.java
@@ -1,14 +1,30 @@
 package com.algorithm.day31;
 
+import java.util.Arrays;
+
 public class PrimeNumber {
     public int solution(int n) {
         int answer = 0;
-        int[] arr = new int[n];
+        int[] arr = new int[n - 1];
 
-        //1. 배열 만들고 n 만큼 생성
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
+        //1. 배열 만들고 1~n 만큼 값 넣기
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 2;
         }
+        System.out.println(Arrays.toString(arr));
+
+        //2. 소수 찾아서 제거
+        for (int i = 0; i * i <= n; i++) {//배열을 전체 돌면서
+            if (arr[i] != 0) {//배열 값이 0이 아닐 때
+                for (int j = i * 2 + 2; j <= arr.length; j += arr[i]) {// 소수 값은 0으로 변경
+                    System.out.println(j);
+                    arr[j] = 0;
+                    answer++;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+
 
         return answer;
     }
