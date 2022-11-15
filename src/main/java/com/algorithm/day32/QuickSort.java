a3ec6e690a3c5271ec1d951eb39c7fca38305573
seoyun <may-primavera@naver.com>
2022-11-15 09:16:53 +0900

    feat: [알고리즘] 퀵 정렬

---
+++ src/main/java/com/algorithm/day32/QuickSort.java
@@ -0,0 +1,15 @@
+package com.algorithm.day32;
+
+import java.util.List;
+
+public class QuickSort {
+    public List<Integer> solution(int[] arr) {
+
+    }
+    public static void main(String[] args) {
+        QuickSort T = new QuickSort();
+        int[] arr = {20, 18, 5, 19, 5, 25, 40, 50};
+
+        System.out.println(T.solution(arr));
+    }
+}
