fcb3997312cd711d29da42bfe0e286c3ddb9ad8f
seoyun <may-primavera@naver.com>
2022-11-15 10:31:06 +0900

    feat: [알고리즘] 퀵 정렬 재귀, list 합치기

--- src/main/java/com/algorithm/day32/QuickSort.java
+++ src/main/java/com/algorithm/day32/QuickSort.java
@@ -7,25 +7,44 @@ import java.util.List;
 // 2. 기준값 기준으로 왼쪽 오른쪽으로 나누어 담는 로직 구현
 // List<Integer>, List<Integer>, List<Integer> 에 담아 보세요.
 public class QuickSort {
-    public List<Integer> solution(int[] arr) {
-        int pivot = arr[arr.length / 2];
-        List<Integer> left = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < pivot) {
-                left.add(arr[i]);
+    //list 합치기
+    public List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right) {
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+        return answer;
+    }
+
+    public List<Integer> solution(List<Integer> arr) {
+        int pivot = arr.get(arr.size() / 2); //5
+        List<Integer> left = new ArrayList<>(); //5
+        List<Integer> right = new ArrayList<>(); //20, 18, 19, 25, 40, 50
+        List<Integer> mid = new ArrayList<>();
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) < pivot) {
+                left.add(arr.get(i));
+            } else if (arr.get(i) > pivot) {
+                right.add(arr.get(i));
             } else {
-                right.add(arr[i]);
+                mid.add(arr.get(i));
             }
         }
-        return;
+
+        //list를 합치는 연산
+        return merge(left, mid, right);
     }
 
     public static void main(String[] args) {
         QuickSort T = new QuickSort();
         int[] arr = {20, 18, 5, 19, 5, 25, 40, 50};
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            al.add(arr[i]);
+        }
 
-        System.out.println(T.solution(arr));
+        System.out.println(T.solution(al));
     }
 }
