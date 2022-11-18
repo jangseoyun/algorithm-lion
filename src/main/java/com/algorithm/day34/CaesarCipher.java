ba8187ac79ecb388829f590686ec030c9cebc580
seoyun <may-primavera@naver.com>
2022-11-18 09:50:07 +0900

    feat: [알고리즘] 시저 암호 공백 고려

--- src/main/java/com/algorithm/day34/CaesarCipher.java
+++ src/main/java/com/algorithm/day34/CaesarCipher.java
@@ -4,16 +4,15 @@ public class CaesarCipher {
     public String solution(String s, int n) {
         String answer = "";
         //공백제거
-        String str = s.replaceAll(" ", "");
 
         //if z 이면 0부터
-        for (Character c : str.toCharArray()) {
-            if (c.equals('z')) {
-                answer += (char) (96 + n);
-            } else if (c.equals('Z')) {
-                answer += (char) (64 + n);
-            } else {
+        for (Character c : s.toCharArray()) {
+            if (c == 90 || c == 122) { //z라면?
+                answer += (char) ((c - 26) + n);
+            } else if (c >= 65 && c <= 90 || c >= 97 && c <= 122) {
                 answer += (char) (c + n);
+            } else {
+                answer += c;
             }
         }
 
@@ -22,8 +21,8 @@ public class CaesarCipher {
 
     public static void main(String[] args) {
         CaesarCipher T = new CaesarCipher();
-        String s = "z";
-        int n = 1;
+        String s = "a B z";
+        int n = 4;
         System.out.println(T.solution(s, n));
     }
 }
