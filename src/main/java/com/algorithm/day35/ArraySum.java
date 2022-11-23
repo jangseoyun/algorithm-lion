61fb52fe1e9549750a0fe35150c097b4f6d9ab48
seoyun <may-primavera@naver.com>
2022-11-23 09:12:23 +0900

    feat: [알고리즘] 재귀함수 배열 요소 더하기

--- src/main/java/com/algorithm/day35/ArraySum.java
+++ src/main/java/com/algorithm/day35/ArraySum.java
@@ -1,16 +1,16 @@
 package com.algorithm.day35;
 
 public class ArraySum {
-    public static int solution(int[] arr, int i, int answer) {
-        System.out.printf("%d %d %d \n", arr.length, i, answer);
+    public static int solution(int[] arr, int i) {
+        System.out.printf("%d %d \n", arr.length, i);
         if (arr.length == i) {
-            return answer;
+            return 0;
         }
-        return solution(arr, i + 1, answer + arr[i]);
+        return arr[i] + solution(arr, i + 1);
     }
 
     public static void main(String[] args) {
         int[] arr = {7, 3, 2, 9};
-        System.out.println(solution(arr, 0, 0));
+        System.out.println(solution(arr, 0));
     }
 }
