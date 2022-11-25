db608e7fbbb93ee2c257c57f39b2610c88a56062
seoyun <may-primavera@naver.com>
2022-11-25 09:17:48 +0900

    feat: [알고리즘] 최대 공약수 구하기

---
+++ src/main/java/com/algorithm/day37/Codeup2623.java
@@ -0,0 +1,15 @@
+package com.algorithm.day37;
+
+import java.util.Scanner;
+
+public class Codeup2623 {
+    public static void solution(int first, int second) {
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        solution(first, second);
+    }
+}
