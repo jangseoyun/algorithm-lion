7df83c9cfaa3130497ee1c0efe6367f0daa96d61
seoyun <may-primavera@naver.com>
2022-11-25 09:39:41 +0900

    feat: [알고리즘] 최대 공약수 구하기 (재귀)

--- src/main/java/com/algorithm/day37/Codeup2623.java
+++ src/main/java/com/algorithm/day37/Codeup2623.java
@@ -3,13 +3,21 @@ package com.algorithm.day37;
 import java.util.Scanner;
 
 public class Codeup2623 {
-    public static void solution(int first, int second) {
-
+    public static int solution(int a, int b) {
+        if (a > b) {
+            return solution(a - b, b);
+        } else if (a < b) {
+            return solution(a, b - a);
+        } else if (a == b) {
+            return a;
+        } else {
+            return 1;
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int first = sc.nextInt();
-        int second = sc.nextInt();
-        solution(first, second);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(solution(a, b));
     }
 }
