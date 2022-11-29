bac516a2222dfdec35c5d2d41b2fe6d977a9fbb9
seoyun <may-primavera@naver.com>
2022-11-29 10:19:36 +0900

    feat: [알고리즘] 최대 자릿수 구하기 / 중복제거 (set)

--- src/main/java/com/algorithm/day38/RadixSort.java
+++ src/main/java/com/algorithm/day38/RadixSort.java
@@ -1,8 +1,23 @@
 package com.algorithm.day38;
 
+import com.algorithm.day19.HashPractice;
+
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class RadixSort {
+    public static HashSet getDigits(int[] arr) {
+        HashSet<Integer> hash = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                hash.add(1);
+            } else {
+                hash.add((int) (Math.log10(arr[i]) + 1));
+            }
+        }
+        System.out.println(hash);
+        return hash;
+    }
     public static int[] solution(int[] arr) {
         int[] redixArr = new int[10];
         Arrays.fill(redixArr, -1);//배열 같은 값 초기화
@@ -12,11 +27,11 @@ public class RadixSort {
         }
 
         System.out.println(Arrays.toString(redixArr));
-
         return redixArr;
     }
     public static void main(String[] args) {
         int[] arr = {7, 4, 5, 9, 1, 0};
         solution(arr);
+        getDigits(arr);
     }
 }
