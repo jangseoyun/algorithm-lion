370903fd230acaa1918af2793465712bb83c4d9b
seoyunjang <syjang0501@gmail.com>
2022-12-09 10:26:21 +0900

    feat: [알고리즘] LCS 다이나믹 프로그래밍

--- src/main/java/com/algorithm/day43/LCS01.java
+++ src/main/java/com/algorithm/day43/LCS01.java
@@ -1,5 +1,7 @@
 package com.algorithm.day43;
 
+import java.util.Arrays;
+
 public class LCS01 {
     public static void main(String[] args) {
         String str1 = "ABCDCBA"; //열
@@ -10,18 +12,23 @@ public class LCS01 {
 
         //str2을 i로 놓고 바깥쪽에서 반복할 에정
         //str1을 j로 놓고 안쪽에서 반복할 예정
-        for (int i = 1; i < str2.length(); i++) {
-            for (int j = 1; j < str1.length(); j++) {
-                System.out.printf("%s %s\n", str2.charAt(i), str1.charAt(j));
+        for (int i = 0; i <= str2.length(); i++) {
+            for (int j = 0; j < str1.length(); j++) {
+                //System.out.printf("%s %s\n", str2.charAt(i), str1.charAt(j));
                 //두개가 같으며 대각선에서 가지고 온다
                 //대각선 dp[i-1][j-1]
-                if (str1.charAt(i) == str2.charAt(j)) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+                else if (str1.charAt(i) == str2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
+                }
+                else {
                     //두개가 다르면 max(위 || 왼쪽)
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
+            System.out.println(Arrays.toString(dp[i]));
         }
 
 
