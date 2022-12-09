6a9093732d47b76e26f4ec2fce0a5db15f44cde0
seoyunjang <syjang0501@gmail.com>
2022-12-09 09:50:27 +0900

    feat: [알고리즘] LCS 다이나믹 프로그래밍

--- src/main/java/com/algorithm/day43/LCS01.java
+++ src/main/java/com/algorithm/day43/LCS01.java
@@ -6,15 +6,24 @@ public class LCS01 {
         String str2 = "DCABDC"; //행
 
         //int size = str2.length() * str1.length();
-        int[][] dp = new int[str2.length()][str1.length()];
+        int[][] dp = new int[str2.length() + 1][str1.length() + 1];
 
         //str2을 i로 놓고 바깥쪽에서 반복할 에정
         //str1을 j로 놓고 안쪽에서 반복할 예정
-        for (int i = 0; i < str2.length(); i++) {
-            for (int j = 0; j < str1.length(); j++) {
+        for (int i = 1; i < str2.length(); i++) {
+            for (int j = 1; j < str1.length(); j++) {
                 System.out.printf("%s %s\n", str2.charAt(i), str1.charAt(j));
+                //두개가 같으며 대각선에서 가지고 온다
+                //대각선 dp[i-1][j-1]
+                if (str1.charAt(i) == str2.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    //두개가 다르면 max(위 || 왼쪽)
+                    dp[i][j] = Math.max(str1.charAt(i), str2.charAt(j));
+                }
             }
         }
+
 
     }
 }
